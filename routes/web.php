76c79da0f9107796fdf5e274ece825af4d877090
CaioBalc <balczarekcaio@gmail.com>
2024-03-21 11:40:26 -0300

    Update

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/queries', function() {
+    #$event = \App\Models\Event::all(); # select * from events
+    #$event = \App\Models\Event::all(['title', 'description']); # select title, description from events 
+    #$event = \App\Models\Event::where('id', 1)->get(); # select * from events where id = 1
+    # or
+    #$event = \App\Models\Event::where('id', 1)->first();
+    #$event = \App\Models\Event::where('id', 1)->get();
+    #$event = \App\Models\Event::find(1);
+
+    #return $event;
+});
+
+Route::get('/queries/{id}', function($event) {
+    $event = \App\Models\Event::find($event);
+
+    return $event;
+});
